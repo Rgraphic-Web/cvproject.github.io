0369db179889c161ce3b8a635ce74d4dc8584744
Ravindu Samarakoon <112361533+Rgraphic-Web@users.noreply.github.com>
2024-01-02 07:36:16 +0530

    reduced  file sizes and added more new things

--- exp/script.js
+++ exp/script.js
@@ -2,3 +2,16 @@ function toggleDiv() {
     var myDiv = document.getElementById("myDiv");
     myDiv.style.display = (myDiv.style.display === "none") ? "table" : "none";
 }
+
+function toggleReadMore() {
+    var longText = document.getElementById('longText');
+    longText.classList.toggle('expanded');
+    var buttonText = document.querySelector('button');
+  
+    if (longText.classList.contains('expanded')) {
+      buttonText.textContent = 'Read Less';
+    } else {
+      buttonText.textContent = 'Read More';
+    }
+  }
+  
