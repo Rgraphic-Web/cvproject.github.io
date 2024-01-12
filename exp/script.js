58c262cbd3d1d5846c38656665a2b3b406c3e4a5
Ravindu Samarakoon <112361533+Rgraphic-Web@users.noreply.github.com>
2024-01-12 07:44:04 +0530

    Did losts of changes
    I did lots of changes and now I think half of the page is done. I'll pull this because need to see what it look on my mobile

--- exp/script.js
+++ exp/script.js
@@ -2,6 +2,25 @@ function toggleDiv() {
     var myDiv = document.getElementById("myDiv");
     myDiv.style.display = (myDiv.style.display === "none") ? "table" : "none";
 }
+function toggleDiv2() {
+  var myDiv2 = document.getElementById("myDiv2");
+  var myDivsub2 = document.getElementById("myDivsub2");
+  var myDivsub3= document.getElementById("myDivsub3");
+  var container2= document.getElementById("container2")
+  if (myDiv2.style.display === "none") {
+    myDiv2.style.display = "table";
+    myDivsub2.style.display = "table";
+    myDivsub3.style.display = "table";
+    myDiv2.style.animation = "G1A 0.3s linear";
+    myDivsub2.style.animation = "G1A 0.3s linear";
+    myDivsub3.style.animation = "G1A 0.3s linear";
+    container2.style.animation = "G1A 0.3s linear";
+  } else {
+    myDiv2.style.display = "none";
+    myDivsub2.style.display = "none";
+    myDivsub3.style.display = "none";
+  }
+}
 
 function toggleReadMore() {
     var longText = document.getElementById('longText');
@@ -14,4 +33,29 @@ function toggleReadMore() {
       buttonText.textContent = 'Read More';
     }
   }
-  
+  function toggleCards() {
+    var cv3 = document.querySelector('.cv3');
+    cv3.classList.toggle('show');
+}
+
+document.getElementById('squar1').addEventListener('mouseover', function() {
+  document.body.style.backgroundImage = "url('Bimage3r.jpg')";
+});
+
+document.getElementById('squar1').addEventListener('mouseout', function() {
+  document.body.style.backgroundImage = "url('Bimage3.jpg')";
+});
+document.getElementById('container2').addEventListener('mouseover', function() {
+  document.body.style.backgroundImage = "url('Bimage3r.jpg')";
+});
+
+document.getElementById('container2').addEventListener('mouseout', function() {
+  document.body.style.backgroundImage = "url('Bimage3.jpg')";
+});
+document.getElementById('container1').addEventListener('mouseover', function() {
+  document.body.style.backgroundImage = "url('Bimage3r.jpg')";
+});
+
+document.getElementById('container1').addEventListener('mouseout', function() {
+  document.body.style.backgroundImage = "url('Bimage3.jpg')";
+});
