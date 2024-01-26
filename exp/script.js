a519f35fb7b8fc832776c251beea9662cd7fdae4
Ravindu Samarakoon <112361533+Rgraphic-Web@users.noreply.github.com>
2024-01-26 07:49:20 +0530

    Did lost of changes and I need to see with different devices

--- exp/script.js
+++ exp/script.js
@@ -1,7 +1,15 @@
 function toggleDiv() {
-    var myDiv = document.getElementById("myDiv");
-    myDiv.style.display = (myDiv.style.display === "none") ? "table" : "none";
+  var myDiv = document.getElementById("myDiv");
+  var container1 = document.getElementById("container1");
+  if (myDiv.style.display === "none") {
+    myDiv.style.display = "table";
+    myDiv.style.animation = "G1A 0.3s linear";
+    container1.style.animation = "G1A 0.3s linear";
+  } else {
+    myDiv.style.display = "none";
+  }
 }
+
 function toggleDiv2() {
   var myDiv2 = document.getElementById("myDiv2");
   var myDivsub2 = document.getElementById("myDivsub2");
@@ -59,3 +67,46 @@ document.getElementById('container1').addEventListener('mouseover', function() {
 document.getElementById('container1').addEventListener('mouseout', function() {
   document.body.style.backgroundImage = "url('Bimage3.jpg')";
 });
+
+
+
+var container4 = document.getElementById('container4');
+var image = document.getElementById('image');
+
+container4.addEventListener('mousemove', function(event) {
+  var mouseX = event.clientX;
+  var mouseY = event.clientY;
+
+  var imageX = (mouseX / window.innerWidth) * 20;
+  var imageY = (mouseY / window.innerHeight) * 20;
+
+  image.style.transform = 'translate(-' + imageX + '%, -' + imageY + '%)';
+});
+
+container4.addEventListener('mouseleave', function() {
+  image.style.transform = 'translate(0, 0)'; // Reset to the initial position
+});
+
+var container4 = document.getElementById('container4');
+var image2 = document.getElementById('image2'); // Updated variable name
+
+container4.addEventListener('mousemove', function(event) {
+  var mouseX = event.clientX;
+  var mouseY = event.clientY;
+
+  var imageX = (mouseX / window.innerWidth) * 5;
+  var imageY = (mouseY / window.innerHeight) * 5;
+
+  image2.style.transform = 'translate(' + imageX + '%)';
+});
+
+container4.addEventListener('mouseleave', function() {
+  image2.style.transform = 'translate(0, 0)'; // Reset to the initial position
+})
+function toggleDiv3() {
+  if (container4.style.display === "none") {
+    container4.style.display = "flex";
+  } else {
+    container4.style.display = "none";
+  }
+}
